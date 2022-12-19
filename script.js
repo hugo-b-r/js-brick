28dc5ba0271c6beccb51a39b348710797bda89c9
LeMoustachu <hugoberthetrambaud@outlook.com>
2022-12-19 11:35:28 +0100

    ball that bounces

---
+++ script.js
@@ -0,0 +1,34 @@
+var canvas = document.getElementById("myCanvas");
+var ctx = canvas.getContext("2d");
+
+var x = canvas.width/2;
+var y = canvas.height-30;
+
+var dx = 2;
+var dy = -2;
+
+var ballRadius = 10;
+
+function drawBall() {
+    ctx.beginPath();
+    ctx.arc(x, y, ballRadius, 0, Math.PI*2);
+    ctx.fillStyle = "#0095DD";
+    ctx.fill();
+    ctx.closePath();
+}
+
+function draw() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drawBall();
+    if (y < ballRadius || y > canvas.height-ballRadius) {
+        dy = -dy;
+    } else if (x < ballRadius || x > canvas.width-ballRadius) {
+        dx = -dx;
+    }
+
+    x += dx;
+    y += dy;
+    
+    
+}
+setInterval(draw, 10);
